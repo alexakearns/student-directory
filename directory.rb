9b6da46b8e0792a6ab071768f77a0d403ae61c17
Alexa <alexakearns@outlook.com>
2020-03-12 13:30:54 +0000

    8. Exercises - Question 5

--- directory.rb
+++ directory.rb
@@ -6,13 +6,22 @@ def input_students
   students = []
   # get the first name
   name = gets.chomp
+  height = gets.chomp
+  birthplace = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add student has to the array
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: :november,
+      birthplace: birthplace, height: height}
     puts "Now we have #{students.count} students"
     # get another name from user
     name = gets.chomp
+    if name.empty?
+      break
+    else
+      height = gets.chomp
+      birthplace = gets.chomp
+    end
   end
   # return array of students
   students
@@ -23,13 +32,10 @@ def print_header
   puts "-------------"
 end
 def print(students)
-  x = 0
-  until x == students.length
-      puts "#{students[x][:name]} (#{students[x][:cohort]} cohort)"
-      x += 1
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort), #{student[:height]}cm from #{student[:birthplace]}."
   end
 end
-
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
